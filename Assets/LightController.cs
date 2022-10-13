35f0dadfa8ea53b7aba1d75f4171b23b6af96d6f
Arvi <xarvichanx@gmail.com>
2022-10-13 12:07:44 +0200

    Little update on calling lights

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -5,57 +5,61 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-    public GameObject player;
     public float speed = 2.0f;
 
-    private bool amIcalled;
+    private GameObject player;
+
     private Vector3 targetDirection;
-    private bool lightEmission;
-    private Vector3 origin = new Vector3(10.0f, 10.0f, 10.0f);
     private Vector3 newTarget;
+
+    private float speedFactor;
+    private bool amIcalled;
 
     void Start()
     {
-        lightEmission = gameObject.GetComponent<Light>().enabled;
+        speedFactor = speed;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
         Vector3 playerPos = player.transform.position;
-        lightEmission = gameObject.GetComponent<Light>().enabled;
 
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             amIcalled = true;
+            speedFactor = speed;
         }
 
-        // this solution is temporary
         SetNewTarget(playerPos);
-        targetDirection = playerPos - transform.position;
-
 
         if (!IsTargetReached(newTarget) && amIcalled)
         {
-            transform.position += targetDirection.normalized * speed * Time.deltaTime;
+            transform.position += targetDirection.normalized * speedFactor * Time.deltaTime;
+            speedFactor *= 1.05f; // let just leave it for now
         }
          
         if (IsTargetReached(newTarget))
         {
+            Destroy(gameObject);
             amIcalled = false;
             gameObject.GetComponent<Light>().enabled = false;
-
         }
 
     }
     bool IsTargetReached(Vector3 target)
     {
         Vector3 distance = transform.position - target;
+
         if(distance.magnitude <= 0.1f)
-        { return true; }
+        { 
+            return true;
+        }
         return false;
     }
     void SetNewTarget(Vector3 target)
     {
         newTarget = target;
+        targetDirection = target - transform.position;
     }
 }
